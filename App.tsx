3da98a746b3d4ee40941b69807ec6c7948b29f67
Carlos Almeida <cosmosdesigner@gmail.com>
2025-07-22 23:03:11 +0100

    move business logic to hook

--- App.tsx
+++ App.tsx
@@ -4,78 +4,23 @@ import { Dashboard } from "./components/Dashboard";
 import { BlockList } from "./components/BlockList";
 import { Modal } from "./components/Modal";
 import { BlockForm } from "./components/BlockForm";
-import { useLocalStorage } from "./hooks/useLocalStorage";
-import { Block, BlockStatus } from "./types";
 import { BlockDetails } from "./components/BlockDetails";
+import useBlocks from "./hooks/useBlocks";
 
 const App: React.FC = () => {
-  const [blocks, setBlocks] = useLocalStorage<Block[]>("blocks", []);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [editingBlock, setEditingBlock] = useState<Block | null>(null);
-  const [viewingBlock, setViewingBlock] = useState<Block | null>(null);
-
-  const handleOpenModal = useCallback(() => {
-    setEditingBlock(null);
-    setIsModalOpen(true);
-  }, []);
-
-  const handleCloseModal = useCallback(() => {
-    setIsModalOpen(false);
-    setEditingBlock(null);
-  }, []);
-
-  const handleSaveBlock = useCallback(
-    (blockData: Omit<Block, "id" | "status">) => {
-      if (editingBlock) {
-        setBlocks((prev) =>
-          prev.map((b) =>
-            b.id === editingBlock.id ? { ...editingBlock, ...blockData } : b
-          )
-        );
-      } else {
-        const newBlock: Block = {
-          id: Date.now().toString(),
-          ...blockData,
-          status: BlockStatus.ONGOING,
-        };
-        setBlocks((prev) => [newBlock, ...prev]);
-      }
-      handleCloseModal();
-    },
-    [editingBlock, setBlocks, handleCloseModal]
-  );
-
-  const handleResolveBlock = useCallback(
-    (id: string) => {
-      setBlocks((prev) =>
-        prev.map((b) =>
-          b.id === id
-            ? {
-                ...b,
-                status: BlockStatus.RESOLVED,
-                endDate: new Date().toISOString(),
-              }
-            : b
-        )
-      );
-    },
-    [setBlocks]
-  );
-
-  const handleDeleteBlock = useCallback(
-    (id: string) => {
-      setBlocks((prev) => prev.filter((b) => b.id !== id));
-    },
-    [setBlocks]
-  );
-
-  const handleViewBlockDetails = useCallback((block: Block | null) => {
-    setViewingBlock(block);
-  }, []);
-
-  const handleCloseDetailsModal = useCallback(() => {
-    setViewingBlock(null);
-  }, []);
+  const {
+    blocks,
+    handleCloseDetailsModal,
+    handleDeleteBlock,
+    handleOpenModal,
+    handleResolveBlock,
+    handleSaveBlock,
+    handleViewBlockDetails,
+    isModalOpen,
+    viewingBlock,
+    editingBlock,
+    handleCloseModal,
+  } = useBlocks();
 
   return (
     <div className="min-h-screen bg-slate-900 font-sans">
---
+++ hooks/useBlocks.tsx
@@ -0,0 +1,87 @@
+import { Block, BlockStatus } from "@/types";
+import { useCallback, useState } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export default function useBlocks() {
+  const [blocks, setBlocks] = useLocalStorage<Block[]>("blocks", []);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingBlock, setEditingBlock] = useState<Block | null>(null);
+  const [viewingBlock, setViewingBlock] = useState<Block | null>(null);
+
+  const handleOpenModal = useCallback(() => {
+    setEditingBlock(null);
+    setIsModalOpen(true);
+  }, []);
+
+  const handleCloseModal = useCallback(() => {
+    setIsModalOpen(false);
+    setEditingBlock(null);
+  }, []);
+
+  const handleSaveBlock = useCallback(
+    (blockData: Omit<Block, "id" | "status">) => {
+      if (editingBlock) {
+        setBlocks((prev) =>
+          prev.map((b) =>
+            b.id === editingBlock.id ? { ...editingBlock, ...blockData } : b
+          )
+        );
+      } else {
+        const newBlock: Block = {
+          id: Date.now().toString(),
+          ...blockData,
+          status: BlockStatus.ONGOING,
+        };
+        setBlocks((prev) => [newBlock, ...prev]);
+      }
+      handleCloseModal();
+    },
+    [editingBlock, setBlocks, handleCloseModal]
+  );
+
+  const handleResolveBlock = useCallback(
+    (id: string) => {
+      setBlocks((prev) =>
+        prev.map((b) =>
+          b.id === id
+            ? {
+                ...b,
+                status: BlockStatus.RESOLVED,
+                endDate: new Date().toISOString(),
+              }
+            : b
+        )
+      );
+    },
+    [setBlocks]
+  );
+
+  const handleDeleteBlock = useCallback(
+    (id: string) => {
+      setBlocks((prev) => prev.filter((b) => b.id !== id));
+    },
+    [setBlocks]
+  );
+
+  const handleViewBlockDetails = useCallback((block: Block | null) => {
+    setViewingBlock(block);
+  }, []);
+
+  const handleCloseDetailsModal = useCallback(() => {
+    setViewingBlock(null);
+  }, []);
+
+  return {
+    blocks,
+    isModalOpen,
+    viewingBlock,
+    editingBlock,
+    handleOpenModal,
+    handleCloseModal,
+    handleSaveBlock,
+    handleResolveBlock,
+    handleDeleteBlock,
+    handleViewBlockDetails,
+    handleCloseDetailsModal,
+  };
+}
